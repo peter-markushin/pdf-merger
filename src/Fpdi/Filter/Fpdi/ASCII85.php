3e9f26759d812f6a1add7017025dfdec271a4991
Peter <piotr-m@piotr-m.net>
2013-03-07 11:47:11 +0300

    Update code to PHP 5.3, rename classes according PSR-0, add composer.json

---
+++ src/Fpdi/Filter/Fpdi/ASCII85.php
@@ -0,0 +1,35 @@
+<?php
+//
+//  FPDI - Version 1.4.3
+//
+//    Copyright 2004-2012 Setasign - Jan Slabon
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+namespace Fpdi\Filter\Fpdi;
+
+class ASCII85 extends \Fpdi\Filter\ASCII85
+{
+    public $fpdi;
+
+    public function __construct(&$fpdi)
+    {
+        $this->fpdi =& $fpdi;
+    }
+
+    public function error($msg)
+    {
+        $this->fpdi->error($msg);
+    }
+}
